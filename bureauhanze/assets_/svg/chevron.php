079225d07be013c8478d2f43272bc5b876beae26
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-10-13 15:50:53 +0200

    update scss

--- bureauhanze/assets_/svg/chevron.php
+++
@@ -1,7 +0,0 @@
-<svg width="15px" height="9px" viewBox="0 0 15 9" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <g id="Projectinrichting" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Home" transform="translate(-1619.000000, -4605.000000)" fill-rule="nonzero" stroke="currentColor" stroke-width="2">
-            <path d="M1632.15683,4605.98623 L1621.31451,4605.98623 C1620.80144,4605.98623 1620.54006,4606.60579 1620.90793,4606.97366 L1626.32908,4612.39482 C1626.55174,4612.61747 1626.9196,4612.61747 1627.14236,4612.39482 L1632.56351,4606.97366 C1632.93128,4606.60579 1632.6699,4605.98623 1632.15683,4605.98623 Z" id="Path-Copy-10" transform="translate(1626.735687, 4609.274020) scale(-1, 1) rotate(180.000000) translate(-1626.735687, -4609.274020) "></path>
-        </g>
-    </g>
-</svg>
--- bureauhanze/assets_/svg/corner.php
+++
@@ -1,8 +0,0 @@
-<svg width="37px" height="37px" viewBox="0 0 37 37" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>Combined Shape</title>
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Home" transform="translate(-1325.000000, -3013.000000)" fill="currentcolor">
-            <path d="M1362,3013 L1362,3018 L1330,3018 L1330,3050 L1325,3050 L1325,3013 L1362,3013 Z"></path>
-        </g>
-    </g>
-</svg>
